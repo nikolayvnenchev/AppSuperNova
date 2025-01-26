9c3abd5a655e7b9151f090122765bfa17c4e4468
Nikolay Nenchev <145499859+nikolayvnenchev@users.noreply.github.com>
2025-01-26 14:26:37 +0200

    Update app.js

--- api/app.js
+++ api/app.js
@@ -9,6 +9,7 @@ import chatRoute from "./routes/chat.route.js";
 import messageRoute from "./routes/message.route.js";
 
 const app = express();
+const url = "https://appsupernova.onrender.com";
 
 app.use(cors({ origin: process.env.CLIENT_URL, credentials: true }));
 app.use(express.json());
@@ -21,6 +22,6 @@ app.use("/api/test", testRoute);
 app.use("/api/chats", chatRoute);
 app.use("/api/messages", messageRoute);
 
-app.listen(8800, () => {
-    console.log("Server is running on port 8800!");
+app.listen(url, () => {
+    console.log("Server is running on port https://appsupernova.onrender.com !");
 })

a85a635d3dea827a3aed9b5f334284fb1d79c723
Nikolay Nenchev <145499859+nikolayvnenchev@users.noreply.github.com>
2025-01-26 17:33:27 +0200

    Update apiRequest.js

--- client/src/lib/apiRequest.js
+++ client/src/lib/apiRequest.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const apiRequest = axios.create({
-  baseURL: "https://appsupernova.onrender.com/api",
+  baseURL: "https://appsupernova.onrender.com",
   withCredentials: true,
 });
 

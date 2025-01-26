f0fa3e860ee5c76c5606f2a4faa5b69cb53407a0
Nikolay Nenchev <145499859+nikolayvnenchev@users.noreply.github.com>
2025-01-26 20:47:16 +0200

    Update auth.cotroller.js

--- api/controllers/auth.cotroller.js
+++ api/controllers/auth.cotroller.js
@@ -67,7 +67,7 @@ export const login = async (req, res) => {
         res
            .cookie("token", token, {
             httpOnly: true,
-            secure:true,
+            secure: process.env.NODE_ENV === 'production',
             sameSite: 'None',
             domain: '.onrender.com',
             maxAge: age,

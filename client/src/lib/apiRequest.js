9c9663aed9b18afb637d918fae8b694b67486460
Nikolay Nenchev <145499859+nikolayvnenchev@users.noreply.github.com>
2025-01-26 18:01:23 +0200

    Update apiRequest.js

--- client/src/lib/apiRequest.js
+++ client/src/lib/apiRequest.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const apiRequest = axios.create({
-  baseURL: "https://imotisupernova.onrender.com/api",
+  baseURL: "https://appsupernova.onrender.com/api",
   withCredentials: true,
 });
 

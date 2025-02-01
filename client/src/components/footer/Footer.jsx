8ffb294120d981670a302c3ac82c02ae2a5eb9e2
nikolayvnenchev <niki93sci@abv.bg>
2025-02-01 21:46:15 +0200

    1st change footer

--- client/src/components/footer/Footer.jsx
+++ client/src/components/footer/Footer.jsx
@@ -2,8 +2,7 @@ import React from 'react';
 
 export default function Footer() {
   return (
-    <footer
-      className="bg-neutral-200 text-center dark:bg-neutral-700 lg:text-left border-t border-neutral-300 dark:border-neutral-600">
+    <footer>
       <div className="p-4 text-neutral-700 dark:text-neutral-200">
         This site was created as an exam project in{' '}
         <a

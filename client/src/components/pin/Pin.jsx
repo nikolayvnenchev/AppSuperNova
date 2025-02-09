31a4c09ec66854d6e566e88c96d590a6673007b1
nikolayvnenchev <niki93sci@abv.bg>
2025-02-09 20:46:05 +0200

    add custom icon to leaflet marker

--- client/src/components/pin/Pin.jsx
+++ client/src/components/pin/Pin.jsx
@@ -1,10 +1,21 @@
 import { Marker, Popup } from "react-leaflet";
+import L from "leaflet";
 import "./pin.scss";
 import { Link } from "react-router-dom";
 
+  // Create a custom icon for the marker
+  const customIcon = new L.Icon({
+    iconUrl: "/marker-icon.png", // Path to your image in the public folder
+    iconSize: [25, 41], // Size of the icon
+    iconAnchor: [12, 41], // Point of the icon which will correspond to marker's location
+    popupAnchor: [1, -34], // Popup anchor, adjust if needed
+    shadowUrl: "/marker-shadow.png", // Optional shadow (if you have it)
+    shadowSize: [41, 41], // Size of the shadow
+  });
+
 function Pin({ item }) {
   return (
-    <Marker position={[item.latitude, item.longitude]}>
+    <Marker position={[item.latitude, item.longitude]} icon={customIcon}>
       <Popup>
         <div className="popupContainer">
           <img src={item.images[0]} alt="" />
